5fa25094cc4784a4eca01e1450115613cef6d4db
juliameylu <julia.m.lu@gmail.com>
2026-04-18 14:41:44 -0700

    lab 3 step 3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,6 +1,6 @@
 // src/MyApp.jsx
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -14,10 +14,41 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  // src/MyApp.jsx (a new function inside the MyApp function)
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
+
+  // src/MyApp.js (a new inner function inside MyApp())
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
+  // src/MyApp.js (a new block inside MyApp())
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="container">
